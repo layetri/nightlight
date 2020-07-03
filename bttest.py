040f2a5cbdff8c3957af6ff60dc50b7d69cd6f3f
layetri <daniel@mainstreamct.com>
2020-07-03 15:53:11 +0200

    updating bluetooth tester

--- bttest.py
+++ bttest.py
@@ -2,14 +2,18 @@ import bluetooth
 
 
 def listen():
-    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-    port = 1
-    server_sock.bind(("", port))
+    port = bluetooth.get_available_port( bluetooth.RFCOMM )
+    server_sock.bind(("",port))
     server_sock.listen(1)
+    print("listening on port %d" % port)
 
-    client_sock, address = server_sock.accept()
-    print("Accepted connection from " + str(address))
+    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+    bluetooth.advertise_service( server_sock, "FooBar Service", uuid )
+
+    client_sock,address = server_sock.accept()
+    print("Accepted connection from ",address)
 
     data = client_sock.recv(1024)
     print("received [%s]" % data)
