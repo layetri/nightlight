9924cfdf547547c4d825c67f07449ade4b30dec9
layetri <daniel@mainstreamct.com>
2020-07-04 14:31:58 +0200

    setup for color

--- homekit.py
+++ homekit.py
@@ -13,7 +13,7 @@ led = RGBLED(26, 6, 5, active_high=True, initial_value=Color(130, 200, 40))
 logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
 
 
-class Light(Accessory):
+class Nightlight(Accessory):
     """Implementation of a mock light accessory."""
 
     category = CATEGORY_LIGHTBULB  # This is for the icon in the iOS Home app.
@@ -25,22 +25,95 @@ class Light(Accessory):
         # If overriding this method, be sure to call the super's implementation first.
         super().__init__(*args, **kwargs)
 
-        # Add the services that this Accessory will support with add_preload_service here
-        serv_light = self.add_preload_service('Lightbulb')
-        self.char_on = serv_light.configure_char('On', value=0)
+        # Setup the Bulb
+        serv_light = self.add_preload_service(
+            'Lightbulb', chars=['On', 'Hue', 'Saturation', 'Brightness'])
 
-        serv_light.setter_callback = self._set_chars
+        # Configure our callbacks
+        self.char_hue = serv_light.configure_char(
+            'Hue', setter_callback=self.set_hue)
+        self.char_saturation = serv_light.configure_char(
+            'Saturation', setter_callback=self.set_saturation)
+        self.char_on = serv_light.configure_char(
+            'On', setter_callback=self.set_state)
+        self.char_on = serv_light.configure_char(
+            'Brightness', setter_callback=self.set_brightness)
 
-    def _set_chars(self, char_values):
-        """This will be called every time the value of the on of the
-        characteristics on the service changes.
+        # Set our instance variables
+        self.accessory_state = 0  # State of the nightlight - On/Off
+        self.hue = 0  # Hue Value 0 - 360 Homekit API
+        self.saturation = 100  # Saturation Values 0 - 100 Homekit API
+        self.brightness = 100  # Brightness value 0 - 100 Homekit API
+
+    def set_state(self, value):
+        self.accessory_state = value
+        if value == 1:  # On
+            self.set_hue(self.hue)
+        else:
+            self.update_color(0, 0, 0)  # Off
+
+    def set_hue(self, value):
+        # Lets only write the new RGB values if the power is on
+        # otherwise update the hue value only
+        if self.accessory_state == 1:
+            self.hue = value
+            rgb_tuple = self.hsv_to_rgb(
+                self.hue, self.saturation, self.brightness)
+            if len(rgb_tuple) == 3:
+                self.update_color(
+                    rgb_tuple[0], rgb_tuple[1], rgb_tuple[2])
+        else:
+            self.hue = value
+
+    def set_brightness(self, value):
+        self.brightness = value
+        self.set_hue(self.hue)
+
+    def set_saturation(self, value):
+        self.saturation = value
+        self.set_hue(self.hue)
+
+    def update_color(self, red, green, blue):
+        led.color = Color(red, green, blue)
+
+    def hsv_to_rgb(self, h, s, v):
         """
-        if "On" in char_values:
-            if char_values["On"] == 1:
-                led.on()
-            elif char_values["On"] == 0:
-                led.off()
-            print('On changed to: ', char_values["On"])
+        This function takes
+         h - 0 - 360 Deg
+         s - 0 - 100 %
+         v - 0 - 100 %
+        """
+
+        hPri = h / 60
+        s = s / 100
+        v = v / 100
+
+        if s <= 0.0:
+            return int(0), int(0), int(0)
+
+        C = v * s  # Chroma
+        X = C * (1 - abs(hPri % 2 - 1))
+
+        rgb_pri = [0.0, 0.0, 0.0]
+
+        if 0 <= hPri <= 1:
+            rgb_pri = [C, X, 0]
+        elif 1 <= hPri <= 2:
+            rgb_pri = [X, C, 0]
+        elif 2 <= hPri <= 3:
+            rgb_pri = [0, C, X]
+        elif 3 <= hPri <= 4:
+            rgb_pri = [0, X, C]
+        elif 4 <= hPri <= 5:
+            rgb_pri = [X, 0, C]
+        elif 5 <= hPri <= 6:
+            rgb_pri = [C, 0, X]
+        else:
+            rgb_pri = [0, 0, 0]
+
+        m = v - C
+
+        return int((rgb_pri[0] + m) * 255), int((rgb_pri[1] + m) * 255), int((rgb_pri[2] + m) * 255)
 
     # The `stop` method can be `async` as well
     def stop(self):
@@ -53,7 +126,7 @@ class Light(Accessory):
 
 def get_accessory(driver):
     """Call this method to get a standalone Accessory."""
-    return Light(driver, 'nightlight')
+    return Nightlight(driver, 'nightlight')
 
 
 # Start the accessory on port 51826
